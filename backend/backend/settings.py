40ffe90cf22282caac91311cffd1cbf2975aa29d
sree581 <sreenandanaanil2004@gmail.com>
2025-10-04 23:42:24 +0530

    feat: Finalize production URLs and CORS policy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS =[
     "http://127.0.0.1:3000",  # We can leave this here
     "http://127.0.0.1:5173", 
     "http://127.0.0.1:5174",
+    "https://kindle-clone-rosy.vercel.app",
 ]
 SITE_ID = 1
 REST_FRAMEWORK = {
