92d60cd99e61a17fd7a6cfdf25a48be03c701803
sree581 <sreenandanaanil2004@gmail.com>
2025-10-08 00:01:29 +0530

    add link

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS =[
     "http://127.0.0.1:5173", 
     "http://127.0.0.1:5174",
     "https://kindle-clone-rosy.vercel.app",
+    "https://kindle-clone-git-main-sreenandanas-projects.vercel.app",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://kindle-clone-rosy.vercel.app",
